19c104972c9156c72e2548ace11520997e82fe81
Ahmed Khalifa <ahmedkhalifa201395@yahoo.com>
2024-04-19 13:55:51 +0200

    forward ref from component to another

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,44 @@
+import { useRef, useState } from "react";
+import ResultModal from "./ResultModal";
+
+export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
+  const dialog = useRef();
+
+  const [timerSrarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  function handleStart() {
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+      dialog.current.showModal();
+    }, targetTime * 1000);
+
+    setTimerStarted(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
+
+  return (
+    <>
+      <ResultModal ref={dialog} result="lost" targetTime={targetTime} />
+      <section className="challenge">
+        <h2>{title}</h2>
+        {timerExpired && <p>You lost!</p>}
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? "s" : ""}
+        </p>
+        <p>
+          <button onClick={timerSrarted ? handleStop : handleStart}>
+            {timerSrarted ? "Stop" : "Start"} Challenge
+          </button>
+        </p>
+        <p className={timerSrarted ? "active" : undefined}>
+          {timerSrarted ? "Time is running..." : "Timer inactive"}
+        </p>
+      </section>
+    </>
+  );
+}
